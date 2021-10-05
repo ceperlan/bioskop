926d6d0b42087371b071f5f10107e36b66fbc0be
Asep Erlan Maulana <ceperlan@Users-MacBook-Pro.local>
2021-10-05 10:28:01 +0700

    v1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:nonton_asep/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 24,
+          right: 24,
+        ),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Home',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 28,
+                    fontWeight: black,
+                  ),
+                ),
+                Text(
+                  'Watch much easier',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: reguler,
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
+            Image.asset(
+              'assets/icon_search.png',
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Image.asset(
-        'assets/image_movie1.png',
+      body: Column(
+        children: [
+          header(),
+        ],
       ),
     );
   }
